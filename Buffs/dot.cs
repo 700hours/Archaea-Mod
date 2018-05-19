e4b18ac86a253797657db38ad44ac6e19c11522e
700hours <31488826+700hours@users.noreply.github.com>
2018-05-19 11:51:56 -0700

    Add files via upload

---
+++ Buffs/dot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArchaeaMod.Buffs
+{
+    public class dot : ModBuff
+    {
+        int ticks;
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            if (npc.active || npc.life > 0)
+            {
+                ticks++;
+                int randDmg = Main.rand.Next(8, 14);
+                bool crit = Main.rand.Next(8) == 0;
+                if(ticks % 150 == 0) npc.StrikeNPC(randDmg, 0f, -1, crit ? true : false, false, false);
+            }
+            else buffIndex--;
+        }
+    }
+}
---
+++ Buffs/magno_cursed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArchaeaMod.Buffs
+{
+    public class magno_cursed : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Granted Inspiration");
+            Description.SetDefault("Free from the curse");
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+    }
+}
